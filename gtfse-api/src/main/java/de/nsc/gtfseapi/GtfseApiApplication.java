907b1cd52fcd12a2926781a9b94b1788bbce97cb
Nicolas Schaber <nicolas.schaber@smateso.com>
2026-06-24 23:24:15 +0200

    chore: initial project setup

---
+++ gtfse-api/src/main/java/de/nsc/gtfseapi/GtfseApiApplication.java
@@ -0,0 +1,13 @@
+package de.nsc.gtfseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtfseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtfseApiApplication.class, args);
+    }
+
+}
