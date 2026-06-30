c56ae5433a0bbd4f20616579c25cb4aa6f2b211b
Nicolas Schaber <nicolas.schaber@smateso.com>
2026-07-01 00:01:23 +0200

    chore: refactor App.java main method to use varargs with final modifier

--- gtfse-api/src/main/java/de/nsc/gtfseapi/App.java
+++ gtfse-api/src/main/java/de/nsc/gtfseapi/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args) {
+    static void main(final String... args) {
         SpringApplication.run(App.class, args);
     }
 
